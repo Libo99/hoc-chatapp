7dd2f3440b659c1761378a14b7b2a77feaeaad6e
Liban Rage <53412720+Libo99@users.noreply.github.com>
2022-02-18 23:07:37 +0100

    initial commit && added splash screen for ios

---
+++ src/screens/Login.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+interface Props {}
+
+const Login = (() => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}) as React.FC;
+
+export default Login;
