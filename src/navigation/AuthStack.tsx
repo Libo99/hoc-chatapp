72d47fd5f6e7b50aa2d9ec289bf299f4d18157e8
Liban Rage <53412720+Libo99@users.noreply.github.com>
2022-02-20 18:43:30 +0100

    removed unnecessary import

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login';
-import ChatStack from './ChatStack';
 
 const AuthStack = () => {
   const Stack = createNativeStackNavigator();
