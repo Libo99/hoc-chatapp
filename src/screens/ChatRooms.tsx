851468741c271365c901c0a48552db44aa83845c
Liban Rage <53412720+Libo99@users.noreply.github.com>
2022-02-20 02:50:03 +0100

    added signout functionality

--- src/screens/ChatRooms.tsx
+++ src/screens/ChatRooms.tsx
@@ -1,15 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { useAuth } from '../context/AuthProvider';
 
 type Props = {};
 
 const ChatRooms = (props: Props) => {
-  const { currentUser } = useAuth();
+  const { currentUser, signOut } = useAuth();
   return (
     <View>
       <Text>Hi {currentUser.email}</Text>
       <Text>ChatRooms</Text>
+      <TouchableOpacity onPress={signOut}>
+      <Text>SignOut</Text>
+      </TouchableOpacity>
     </View>
   );
 };
