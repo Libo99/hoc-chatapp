abb182f246347a0771de9359d1fa3ed98d94b517
Liban Rage <53412720+Libo99@users.noreply.github.com>
2022-02-21 17:38:44 +0100

    added the social buttons to login screen and added an image

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,20 +1,79 @@
-import { View, Text, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Platform,
+} from 'react-native';
 import React from 'react';
-import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import { LoginButton } from 'react-native-fbsdk-next';
-
 import { useAuth } from '../context/AuthProvider';
+import SocialButton from '../components/SocialButton/SocialButton';
 
 const Login = (() => {
-  const { googleLogin, facebookLogin, currentUser } = useAuth();
+  const { googleLogin, facebookLogin } = useAuth();
 
   return (
-    <View>
-      <Text>Login</Text>
-      <GoogleSigninButton onPress={googleLogin} />
-      <Button title="fb login" onPress={facebookLogin} />
+    <View style={styles.container}>
+      <View style={styles.logocontainer}>
+        <ImageBackground
+          source={require('../../assets/logo2.png')}
+          style={styles.logo}
+        >
+          <Text style={styles.logotext}>Rage Chat</Text>
+        </ImageBackground>
+        <Text style={styles.sublogotext}>Connect with Friends and Family</Text>
+      </View>
+      <View style={styles.buttonscontainer}>
+        <SocialButton
+          type="google"
+          title="Log in with Google"
+          onPress={googleLogin}
+        />
+        <SocialButton
+          type="facebook"
+          title="Log in with Facebook"
+          onPress={facebookLogin}
+        />
+      </View>
     </View>
   );
 }) as React.FC;
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  logocontainer: {
+    flex: 2,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 300,
+    width: 400,
+    resizeMode: 'contain',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  logotext: {
+    fontSize: 27,
+    fontWeight: '700',
+    color: 'green',
+  },
+  sublogotext: {
+    fontSize: 15,
+    fontWeight: '400',
+    marginTop: 2,
+    color: 'black',
+  },
+  buttonscontainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+});
