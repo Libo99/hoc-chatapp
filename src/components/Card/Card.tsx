64fda6ef3feee5cbefcefd5d30b87efdd1ca93d3
Liban Rage <53412720+Libo99@users.noreply.github.com>
2022-02-22 19:28:27 +0100

    borderbottomwidth instead of borderwidth

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     marginVertical: 5,
     borderColor: 'lightgrey',
-    borderWidth: 0.6,
+    borderBottomWidth: 0.6,
     paddingBottom: 3,
   },
   cardtitle: {
